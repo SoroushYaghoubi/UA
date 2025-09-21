40bd51455432d80e4b5cc2f447cef86a2b14cffb
Soroush Yaghoubi <soroush.yaghoubi@tu-dortmund.de>
2025-09-21 18:52:40 +0200

    some random stuff

--- presentation/animations.py
+++
@@ -1,7 +0,0 @@
-from manim import *
-
-class MyFirstScene(Scene):
-    def construct(self):
-        text = Text("WELCOME TO TROPICAL GAUSSIANS!")
-        self.play(Write(text))
-        self.wait(2)
